80f16c7bd2cc6e17d1f5ab7fedb2dfddcc5b3611
aic <aikawamasataka@gmail.com>
2017-11-21 23:49:00 +0900

    複数同じメッセージ制御

--- src/main/java/jp/co/jrits/yoyakutter_bot/main/Yoyakutter.java
+++ src/main/java/jp/co/jrits/yoyakutter_bot/main/Yoyakutter.java
@@ -1,52 +1,61 @@
 package jp.co.jrits.yoyakutter_bot.main;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import org.riversun.slacklet.Slacklet;
 import org.riversun.slacklet.SlackletRequest;
 import org.riversun.slacklet.SlackletResponse;
 import org.riversun.slacklet.SlackletService;
+import org.riversun.slacklet.SlackletSession;
 
 public class Yoyakutter {
+	private static HashMap<SlackletSession, String> sessionMap;
 
     public static void main(String[] args) throws IOException {
 
         String botToken = ResourceBundle.getBundle("credentials").getString("slack.bot_api_token");
+        sessionMap = new HashMap<>();
 
         SlackletService slackService = new SlackletService(botToken);
 
         slackService.addSlacklet(new Slacklet() {
 
-
             @Override
             public void onDirectMessagePosted(SlackletRequest req, SlackletResponse resp) {
-                boolean mensionFlg = false;
+            	String oContent = sessionMap.get(req.getSession());
+            	if (oContent == null || !oContent.equals(req.getContent())) {
+            		sessionMap.put(req.getSession(), req.getContent());
 
-                try {
-                    ReplyMessageHandler handler = new ReplyMessageHandler();
-                    handler.reply(req, resp,mensionFlg);
-                } catch (IOException e) {
-                    // TODO 自動生成された catch ブロック
-                    e.printStackTrace();
-                }
+	                boolean mensionFlg = false;
 
-
+	                try {
+	                    ReplyMessageHandler handler = new ReplyMessageHandler();
+	                    handler.reply(req, resp,mensionFlg);
+	                } catch (IOException e) {
+	                    // TODO 自動生成された catch ブロック
+	                    e.printStackTrace();
+	                }
+            	}
             }
 
             @Override
             public void onMentionedMessagePosted(SlackletRequest req, SlackletResponse resp) {
                 // あるチャンネルでこのBOTへのメンション付きメッセージがポストされた(例　「@smilebot おはよう」）
-                boolean mensionFlg = true;
+            	String oContent = sessionMap.get(req.getSession());
+            	if (oContent == null || !oContent.equals(req.getContent())) {
+            		sessionMap.put(req.getSession(), req.getContent());
+	                boolean mensionFlg = true;
 
-                try {
-                    ReplyMessageHandler handler = new ReplyMessageHandler();
-                    handler.reply(req, resp,mensionFlg);
-                } catch (IOException e) {
-                    // TODO 自動生成された catch ブロック
-                    e.printStackTrace();
-                }
-
+	                try {
+	                    ReplyMessageHandler handler = new ReplyMessageHandler();
+	                    handler.reply(req, resp,mensionFlg);
+	                } catch (IOException e) {
+	                    // TODO 自動生成された catch ブロック
+	                    e.printStackTrace();
+	                }
+            	}
             }
 
         });
